288d38bc1ca3d91b511ecce3273c27914208e573
velvet-halation <buizark@gmail.com>
2019-10-11 16:14:10 -0700

    load in mod data

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # bot.py
 import os
-
+import json
 import discord
 from dotenv import load_dotenv
 
@@ -8,9 +8,17 @@ load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
+cirnoData = None
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    print('Loading mod data')
+    loadData(cirnoData, 'cirno')
+
+def loadData(dataField, modId):
+    with open(f'data/{modId}/items.json') as json_file:
+        dataField = json.load(json_file)
+    print('Loaded modId!')
 
 client.run(token)
