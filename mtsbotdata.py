f2a539b1af592c8ac933b0f9d84868483e25ae68
velvet-halation <buizark@gmail.com>
2019-10-13 05:16:40 -0700

    aliases added

--- mtsbotdata.py
+++ mtsbotdata.py
@@ -2,5 +2,29 @@ aliases = {
     'replaythespire': [
         'replay',
         'rts'
+    ],
+    'marimod': [
+        'mari'
+    ],
+    'beakedthecultist-sts': [
+        'beaked'
+    ],
+    'blackruse': [
+        'servant',
+        'the servant',
+        'theservant'
+    ],
+    'gluttonmod': [
+        'glutton'
+    ],
+    'hollowmod': [
+        'hollow knight',
+        'bug knight'
+    ],
+    'madsciencemod': [
+        'mad scientist'
+    ],
+    'pokerplayer': [
+        'poker'
     ]
 }
