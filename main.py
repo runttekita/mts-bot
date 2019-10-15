f907a0b2e531d6813391fa9507e579c9e0841f51
= <benzastawnik@gmail.com>
2019-10-15 15:53:03 -0400

    Add modder command to get list of mods by a given author

--- main.py
+++ main.py
@@ -183,10 +183,25 @@ async def do_command(channel, tokenized_message):
         "bug": dm_modder,
         "feedback": dm_modder,
         "find": find,
+        "modder": get_mods_by_author
     }
     callback = commands.get(tokenized_message[0])
     await callback(channel, tokenized_message)
 
+
+@client.event
+async def get_mods_by_author(channel, tokenized_message):
+    mod_list = []
+    author_name = "reina" if tokenized_message[1] == "reina" else None
+    for mod in Mod_Data("").data:
+        if "mod" in mod and "authors" in mod["mod"] and tokenized_message[1] in [author.lower() for author in mod["mod"]["authors"]]:
+            mod_list.append("`%s`" % mod["mod"]["name"])
+            if author_name is None:
+                author_name = mod["mod"]["authors"][[author.lower() for author in mod["mod"]["authors"]].index(tokenized_message[1])] # Ugly way to get the correct caps for a name since tokenized_message is always lowercase
+    if len(mod_list) == 0:
+        await channel.send("**%s** does not have any mods" % tokenized_message[1])
+    else:
+        await channel.send("**%s**\n%s" % (author_name, "  ".join(mod_list)))
 
 @client.event
 async def find(channel, tokenized_message):
