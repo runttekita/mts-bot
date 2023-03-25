f57d8b1be3fd9a4d5751b25749174865a390332c
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2023-03-24 17:40:56 -0700

    I'm an idiot

--- main.py
+++ main.py
@@ -332,16 +332,14 @@ async def wiki(channel, tokenized_message, discord_message):
     page = " ".join(tokenized_message[index:])
     if wiki_site is not None:
         wiki_page = wiki_site + page
-        response = await page_exists(wiki_page)
-        if response:
-            await send_with_ping(response, discord_message)
+        if page_exists(wiki_page):
+            await send_with_ping(wiki_page, discord_message)
             return
     else:
         for site in wikis.values():
             wiki_page = site + page
-            response = await page_exists(wiki_page)
-            if response:
-                await send_with_ping(response, discord_message)
+            if page_exists(wiki_page):
+                await send_with_ping(wiki_page, discord_message)
                 return
     await discord_message.add_reaction("📑")
     await discord_message.add_reaction("❌")
@@ -1159,9 +1157,8 @@ def is_modding_channel(channel):
 
 
 async def page_exists(wiki_page):
-    response = await session.head(wiki_page)
-    #return response.status < 300 and "<title>Home" not in response.text()
-    return await response.text()
+    response = await session.get(wiki_page)
+    return response.status < 300 and "<title>Home" not in response.text()
 
 
 async def send_with_ping(message, discord_message):
